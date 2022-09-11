f165f3af8310ae79e02a659de715fb3ee8a5005a
nikhil1947 <nikhilrajani20@gnu.ac.in>
2022-09-11 17:56:47 +0530

    nik

---
+++ app/src/main/java/com/example/madpractical3_20012011145/LoginActivity.kt
@@ -0,0 +1,40 @@
+package com.example.madpractical3_20012011145
+import android.content.Intent
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import com.example.madpractical3_20012011145.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window,false)
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        val signup = findViewById<TextView>(R.id.signup)
+
+        signup.setOnClickListener {
+
+            Intent(this, RegActivity::class.java).also {startActivity(it)}
+
+            signup.movementMethod = LinkMovementMethod.getInstance();
+
+        }
+
+        binding.bottomNavigationView.selectedItemId = R.id.bottom_nav_login
+        binding.bottomNavigationView.setOnItemSelectedListener{ it ->
+            when(it.itemId){
+                R.id.bottom_nav_reg -> {
+                    Intent(this, RegActivity::class.java).also {startActivity(it)}
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+}
